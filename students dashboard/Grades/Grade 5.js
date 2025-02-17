96c2c1807aad4c4d9c38f4984bb5549919026ba6
Kanyadetschool <oduorgeofrey141@gmail.com>
2025-02-17 17:08:49 +0300

    mega 8 AND 9 STUDE UPDATE

---
+++ students dashboard/Grades/Grade 5.js
@@ -0,0 +1,32 @@
+// GRADE 5 STUDENTS 2024
+export const Grade5 = [
+  {
+    CurentGrade: "Grade 5",
+    StudentFullName: "",
+     Status:"",
+    AdmissionNo: "",
+    AssessmentNumber: "",
+    EntryNo: "",
+    Gender: "Male",
+    classfilteringbyGender: "",
+    classfilteringCode: "",
+    Disability: "",
+    DateOfAdm: "🕸️",
+    AdmissionClass: "🕸️",
+    DateOfBirth: "",
+    UPI: "🕸️",
+    PhoneNumber: "🕸️",
+    StudentSchoolEmail: "🕸️",
+    ClassTeacher: "🕸️",
+    FathersName: "🕸️",
+    FathersPhoneNumber: "🕸️",
+    MothersName: "🕸️",
+    MothersPhoneNumber: "🕸️",
+    Siblings: "No1:Tom allan \n No2: Joy amalia",   FileUrl1: "",   FileUrl2: "",  FileUrl3: "🕸️",   FileUrl4: "../img/Students.jpg",
+  },
+   
+    // Add more default data objects as needed
+  ];
+  
+   
+  
---
+++ students dashboard/Grades/Grade 6.js
@@ -0,0 +1,32 @@
+// GRADE 5 STUDENTS 2024
+export const Grade6 = [
+  {
+    CurentGrade: "Grade 5",
+    StudentFullName: "PAUL ANTONE",
+     Status:"",
+    AdmissionNo: "",
+    AssessmentNumber: "",
+    EntryNo: "",
+    Gender: "male",
+    classfilteringbyGender: "",
+    classfilteringCode: "",
+    Disability: "",
+    DateOfAdm: "🕸️",
+    AdmissionClass: "🕸️",
+    DateOfBirth: "",
+    UPI: "🕸️",
+    PhoneNumber: "🕸️",
+    StudentSchoolEmail: "🕸️",
+    ClassTeacher: "🕸️",
+    FathersName: "🕸️",
+    FathersPhoneNumber: "🕸️",
+    MothersName: "🕸️",
+    MothersPhoneNumber: "🕸️",
+    Siblings: "No1:Tom allan \n No2: Joy amalia",   FileUrl1: "",   FileUrl2: "",  FileUrl3: "🕸️",   FileUrl4: "../img/Students.jpg",
+  },
+   
+    // Add more default data objects as needed
+  ];
+  
+   
+  
